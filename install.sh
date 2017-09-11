d1b97a09be745eb721bfc740934b9372bee7cc20
Semmy Purewal <semmy.purewal@gmail.com>
2017-09-10 19:32:46 -0700

    add tmux-related stuff

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# following line borrwed from
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "running install script from $DIR"
+
 if [ -e $HOME/.emacs ]; then
     echo "...backing up .emacs file to .emacs.BACKUP"
     mv $HOME/.emacs $HOME/.emacs.BACKUP
@@ -25,3 +24,11 @@ fi
 
 echo "../linking .emacs.d/lib"
 ln -s $DIR/emacs/lib $HOME/.emacs.d/lib
+
+if [ -e $HOME/.tmux.conf ]; then
+    echo "...backing up .tmux.conf file to .tmux.conf.BACKUP"
+    mv $HOME/.tmux.conf $HOME/.tmux.conf.BACKUP
+fi
+
+echo "...linking .tmux.conf"
+ln -s $DIR/tmux/tmux.conf $HOME/.tmux.conf
